313a787eedab527f67a482a2f00b9c3f071cf058
Naresh Kumar <mkumar@firstderivative.com>
2025-09-08 14:23:31 +0100

    initial create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCApplicatiobn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
